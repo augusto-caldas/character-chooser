1dcea2b771e5b1f55de8d3394860196fa2e82101
Luis Augusto Caldas <augusto@luiscaldas.com>
2023-07-03 00:26:00 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# champion_chooser
+ Randomly choose a champion from a file to players
---
+++ main.c
@@ -0,0 +1,16 @@
+// Randomly choose a champion for each player
+// Champion are read from file and players requested on the fly
+// Version: 07_03_2023
+
+#include <stdio.h>
+
+
+int main(){
+    FILE *playersList, *championsList;
+
+    playersList = fopen("players.txt", "r");
+
+
+
+    return 1;
+}
